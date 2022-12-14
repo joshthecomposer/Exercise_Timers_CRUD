170a5429d2db4be465aab147178c8090c33cfb4a
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2022-12-14 09:12:50 -0500

    added temporary buttons to timer view so I can start JS coding

---
+++ flask_app/static/scripts/script.js
@@ -0,0 +1,7 @@
+countdown = document.getElementById('countdown');
+playBtn = document.getElementById('play');
+pauseBtn = document.getElementById('pause');
+
+playBtn.addEventListener('click')
+pauseBtn.addEventListener('click')
+
