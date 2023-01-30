3dd6eddf15ef931c9599e6498a53e9b527fca627
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-01-30 15:25:57 -0500

    Update mysqlconnection.py

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -1,14 +1,8 @@
 import pymysql.cursors
 class MySQLConnection:
     def __init__(self, db):
-        connection = pymysql.connect(host = 'containers-us-west-172.railway.app',
-                                    user = 'root', # change the user and password as needed
-                                    port = 6652,
-                                    password = 'zcvvmSl45TNbzCJJq5e3', 
-                                    db = db,
-                                    charset = 'utf8mb4',
-                                    cursorclass = pymysql.cursors.DictCursor,
-                                    autocommit = True)
+        connection
+        
         self.connection = connection
     def query_db(self, query, data=None):
         with self.connection.cursor() as cursor:
