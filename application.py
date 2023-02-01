e01a7ee71c397a78d56b0c000b5817a84b0b5489
Joshua Wise <joshuawise@joshuas-mbp.myfiosgateway.com>
2023-02-01 13:18:52 -0500

    enable dotenv

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask_app import application
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 from flask_app.controllers import users, main_controller, timers, states
 
 if __name__ == '__main__':
