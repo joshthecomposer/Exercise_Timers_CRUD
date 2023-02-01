ce4d0c8a81b2e0a93005d8825ceb61a9ea1ea09e
Joshua Wise <joshuawise@joshuas-mbp.myfiosgateway.com>
2023-02-01 12:49:00 -0500

    setting up stuff for deploy

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
-from flask_app import application as app
+from flask_app import application
+from dotenv import load_dotenv
+load_dotenv()
 from flask_app.controllers import users, main_controller, timers, states
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
